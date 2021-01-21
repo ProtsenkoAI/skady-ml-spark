0a12ca8e58e688c52afa926e58d5776ff03d04e7
Protsenko Arseny <procenko_03@mail.ru>
2021-01-21 09:50:54 +0300

    added None processing to match_estimator

--- service/manipulators/match_estimator.py
+++ service/manipulators/match_estimator.py
@@ -1,5 +1,7 @@
 class MatchEstimator:
     def get_match_val(self, groups1, groups2):
+        if self._check_none_input(groups1, groups2):
+            return None
         groups1, groups2 = set(groups1), set(groups2)
 
         intersect_elems = groups1.intersection(groups2)
@@ -8,3 +10,9 @@ class MatchEstimator:
         union = len(union_elems)
 
         return intersection / union
+
+    def _check_none_input(self, *inputs):
+        for inp in inputs:
+            if inp is None:
+                return True
+        return False
